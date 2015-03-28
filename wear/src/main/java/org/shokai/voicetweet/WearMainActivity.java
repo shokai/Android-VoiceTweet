baf1569f6d5650b6d192bbfc455ac77ca7b8e37b
Sho Hashimoto <hashimoto@shokai.org>
2015-03-28 16:03:13 +0900

    rename MainActivity -> WearMainActivity
    add Button

--- wear/src/main/java/org/shokai/voicetweet/WearMainActivity.java
+++ wear/src/main/java/org/shokai/voicetweet/WearMainActivity.java
@@ -3,11 +3,15 @@ package org.shokai.voicetweet;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class WearMainActivity extends Activity {
 
     private TextView mTextView;
+    private TextView mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,13 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                mButton = (Button) stub.findViewById(R.id.button);
+                mButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(this, "click", Toast.LENGTH_SHORT)
+                    }
+                });
             }
         });
     }
