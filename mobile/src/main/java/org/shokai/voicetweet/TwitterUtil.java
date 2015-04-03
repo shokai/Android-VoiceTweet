dfb1029e1197d832f5b98fbddc9c47e03efa492e
Sho Hashimoto <hashimoto@shokai.org>
2015-04-03 13:01:50 +0900

    refactor TwitterUtil.java

--- mobile/src/main/java/org/shokai/voicetweet/TwitterUtil.java
+++ mobile/src/main/java/org/shokai/voicetweet/TwitterUtil.java
@@ -9,18 +9,17 @@ import java.util.Properties;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.OAuth2Token;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterUtil {
 
-    private Context mContext;
-    private Properties mProperties;
-    private SharedPreferences mSharedPreferences;
+    private Context mContext; // Activity or Service
+    private Properties mProperties; // save consumer key & secret
+    private SharedPreferences mSharedPreferences; // save access token & secret
 
     public TwitterUtil(Context context){
         mContext = context;
+        mSharedPreferences = mContext.getSharedPreferences("twitter", Context.MODE_PRIVATE);
         mProperties = new Properties();
         try {
             InputStream is = mContext.getResources().openRawResource(R.raw.twitter4j);
@@ -52,25 +51,20 @@ public class TwitterUtil {
         return mProperties.getProperty("oauth.consumerSecret");
     }
 
-    public SharedPreferences getPreference(){
-        if(mSharedPreferences != null) return mSharedPreferences;
-        return mContext.getSharedPreferences("twitter", Context.MODE_PRIVATE);
-    }
-
     public String getAccessToken(){
-        return getPreference().getString("ACCESS_TOKEN", null);
+        return mSharedPreferences.getString("ACCESS_TOKEN", null);
     }
 
     public String getAccessTokenSecret(){
-        return getPreference().getString("ACCESS_TOKEN_SECRET", null);
+        return mSharedPreferences.getString("ACCESS_TOKEN_SECRET", null);
     }
 
     public void setAccessToken(String token){
-        getPreference().edit().putString("ACCESS_TOKEN", token).commit();
+        mSharedPreferences.edit().putString("ACCESS_TOKEN", token).apply();
     }
 
     public void setAccessTokenSecret(String tokenSecret){
-        getPreference().edit().putString("ACCESS_TOKEN_SECRET", tokenSecret).commit();
+        mSharedPreferences.edit().putString("ACCESS_TOKEN_SECRET", tokenSecret).apply();
     }
 
     public void logout(){
