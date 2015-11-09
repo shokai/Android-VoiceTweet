feae14c3135f5e9feda82d79f942f5a702dd4b05
Sho Hashimoto <hashimoto@shokai.org>
2015-11-10 04:29:33 +0900

    use AndroidAnnotation in MainActivity  #16 - OptionsMenu - getting Twitter name & icon in MainActivity

--- mobile/src/main/java/org/shokai/voicetweet/MainActivity.java
+++ mobile/src/main/java/org/shokai/voicetweet/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,7 +12,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.OptionsMenuItem;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.IOException;
@@ -25,6 +29,7 @@ import twitter4j.TwitterException;
 import twitter4j.User;
 
 @EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.menu_main)
 public class MainActivity extends Activity {
 
     private final static String TAG = "MainActivity";
@@ -32,13 +37,18 @@ public class MainActivity extends Activity {
 
     private TwitterUtil mTwitterUtil;
     private Twitter mTwitter;
-    private String mScreenName;
 
     @ViewById(R.id.imageViewProfile)
     ImageView mImageViewProfile;
 
     @ViewById(R.id.textViewScreenName)
     TextView mTextViewScreenName;
+
+    @OptionsMenuItem(R.id.action_tweet_test)
+    MenuItem mItemTweetTest;
+
+    @OptionsMenuItem(R.id.action_login)
+    MenuItem mItemLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +61,7 @@ public class MainActivity extends Activity {
         super.onResume();
         if(mTwitterUtil.hasToken()){
             mTwitter = mTwitterUtil.getTwitterInstance();
-            displayTwitterScreenName();
+            getTwitterScreenNameAsync();
         }
         else{
             mTextViewScreenName.setText(getResources().getText(R.string.text_screen_name));
@@ -60,87 +70,62 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem itemTweetTest = menu.findItem(R.id.action_tweet_test);
-        itemTweetTest.setVisible(BuildConfig.DEBUG && mTwitterUtil.hasToken());
-
-        MenuItem itemLogin = menu.findItem(R.id.action_login);
-        itemLogin.setTitle(mTwitterUtil.hasToken() ? "Logout" : "Login");
+        mItemTweetTest.setVisible(BuildConfig.DEBUG && mTwitterUtil.hasToken());
+        mItemLogin.setTitle(mTwitterUtil.hasToken() ? "Logout" : "Login");
         return super.onPrepareOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_login:
-                Intent intent = new Intent(this, TwitterOAuthActivity.class);
-                startActivityForResult(intent, CODE_TWITTER_LOGIN);
-                return true;
-            case R.id.action_tweet_test:
-                startActivity(new Intent(MainActivity.this, TweetTestActivity_.class));
-                return true;
+    @OptionsItem(R.id.action_login)
+    void login(){
+        Intent intent = new Intent(this, TwitterOAuthActivity.class);
+        startActivityForResult(intent, CODE_TWITTER_LOGIN);
+    }
+
+    @OptionsItem(R.id.action_tweet_test)
+    void tweetTest(){
+        TweetTestActivity_.intent(this).start();
+    }
+
+    @Background
+    void getTwitterScreenNameAsync(){
+        if(mTwitter == null) return;
+        try {
+            String name = mTwitter.getScreenName();
+            displayTwitterScreenName(name);
+            getTwitterProfileImageAsync(name);
+        } catch (TwitterException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    private void displayTwitterScreenName() {
-        if(mTwitter == null) return;
-        new AsyncTask<Void, Void, String>(){
-            @Override
-            protected String doInBackground(Void... params) {
-                try {
-                    return mScreenName = mTwitter.getScreenName();
-                } catch (TwitterException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-
-            @Override
-            protected void onPostExecute(String screen_name) {
-                if(mScreenName == null) return;
-                mScreenName = screen_name;
-                Log.i(TAG, "screen_name: " + screen_name);
-                mTextViewScreenName.setText("@" + screen_name);
-                displayTwitterProfileImage(screen_name);
-            }
-        }.execute();
+    @UiThread
+    void displayTwitterScreenName(String name){
+        Log.i(TAG, "screen_name: " + name);
+        mTextViewScreenName.setText("@" + name);
     }
 
-    private void displayTwitterProfileImage(final String screen_name){
-        if(mTwitter == null) return;
-        new AsyncTask<Void, Void, Bitmap>(){
-            @Override
-            protected Bitmap doInBackground(Void... params) {
-                try {
-                    User user = mTwitter.showUser(screen_name);
-                    URL imageUrl = new URL(user.getBiggerProfileImageURL());
-                    return BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (TwitterException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
+    @Background
+    void getTwitterProfileImageAsync(String screenName) {
+        try {
+            User user = mTwitter.showUser(screenName);
+            URL imageUrl = new URL(user.getBiggerProfileImageURL());
+            Bitmap profileImage = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
+            displayTwitterProfileImage(profileImage);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-            @Override
-            protected void onPostExecute(Bitmap profileImage) {
-                if(profileImage == null) return;
-                Log.i(TAG, "load profile image");
-                mImageViewProfile.setImageBitmap(profileImage);
-                mImageViewProfile.setVisibility(View.VISIBLE);
-            }
-        }.execute();
+    @UiThread
+    void displayTwitterProfileImage(Bitmap image){
+        Log.i(TAG, "load profile image");
+        mImageViewProfile.setImageBitmap(image);
+        mImageViewProfile.setVisibility(View.VISIBLE);
     }
 
 }
